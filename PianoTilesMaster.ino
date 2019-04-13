a9f774a067e570091227d72bd95ee670235a4818
Funky <dominik.ricanek@gmail.com>
2019-04-13 19:35:51 +0200

    codes finished nows the time for tuning

--- PianoTilesMaster.ino
+++ PianoTilesMaster.ino
@@ -1,52 +1,73 @@
 #include "header.h"
 //Konstatny a IO obvody
-static const unsigned long l = 28000;
+static const unsigned long l1 = 28000;
+static const unsigned long l2 = 23000;
 //vzdalenost mezi senzorem a prstikem, premerit
 static const unsigned long s = 30500;
-static const char Sen[] = {A0, A1, A2, A3};
-static const char Pin[] = {2, 3, 4, 5};
+static const int Sen[] = {A0, A1, A2, A3};
+static const int Pin[] = {2, 3, 4, 5};
+static const int bttn = 12;
+unsigned long curTime;
 
 //Promenne programu
 //tresholdy budou muset byt dva (funkce pres pointery)
 int upTrsh = 0;
 int lowTrsh = 0;
 
+//Ulozeni pole objektu sensor pres construtor sensor::sensor(int Sen, int Pin)
+sensor senArray[4]{{Sen[0],Pin[0]},{Sen[1],Pin[1]},{Sen[2],Pin[2]},{Sen[3],Pin[3]}};
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  pinMode(bttn, INPUT_PULLUP);
   
-  //Nastaveni vstupnich pinu
+  //Nastaveni vstupnich pinu senzoru
   for(int i = 0; i < 4; ++i){
     pinMode(Sen[i], INPUT);
   }
   
-  //Nastaveni vystupnich pinu
+  //Nastaveni vystupnich pinu solenoidu
   for(int i = 0; i < 4; ++i){
     pinMode(Pin[i], OUTPUT);
     digitalWrite(Pin[i], LOW);
   }
   delay(100);
+  //Kalibrace pomoci tlacitka
+  calib(Sen[0],Pin[0], bttn, &upTrsh, &lowTrsh);
+  Serial.print("Upper trsh: ");
+  Serial.println(upTrsh);
+  Serial.print("Lower trsh: ");
+  Serial.println(lowTrsh);
+  delay(100);
+  //Startovni nastaveni
+  for(int i = 0; i < 4; ++i){
+    senArray[i].start(bttn, lowTrsh, upTrsh);
+  }
 }
-//Ulozeni pole objektu sensor pres construtor sensor::sensor(int Sen, int Pin)
-sensor senArray[4]{{Sen[0],Pin[0]},{Sen[1],Pin[1]},{Sen[2],Pin[2]},{Sen[3],Pin[3]}};
+
+
 
 void loop() {
-  for(int i = 0; i < 4; ++i){
-    
-    //analogRead a Serial.print v metodach
-    senArray[i].edge(upTrsh, lowTrsh);
+  curTime = millis();
+  
+ //analogRead a Serial.print v metodach
+  senArray[0].edge(upTrsh, lowTrsh);
+  
+    for(int i = 0; i < 5; ++i){
+       senArray[0].vecSpeed(i, l1, l2, s);
+       
+       if((curTime >= senArray[0].pole[i].pushTime)&&(senArray[0].pole[i].pushTime!=0)){
+         digitalWrite(senArray[0].pinOut, HIGH);
+         senArray[0].pole[i].pushTime = 0;
+       }
+       if((curTime >= senArray[0].pole[i].pullTime)&&(senArray[0].pole[i].pullTime!=0)){
+         digitalWrite(senArray[0].pinOut, LOW);
+         senArray[0].pole[i].pullTime = 0;
+       }
+    }
+   
     
     //i pro identifikaci senzoru pri debuggingu
-    senArray[i].tisk(i);
-  }
-  delay(1);
+    senArray[0].tisk(0);
 }
-
-/*
- * #Kalibrace senzoru pomoci tlacitka, naprogramovat startovaci sekvenci v headeru
- * 
- * #Nakoupit trimry na kalibraci jednotlivych senzoru
- * 
- * #Dopsat header.h
- * Dodělat edge metodu, vypocet rychlosti a casy stisku a odepnuti
- * Napsat funkci pro vypocet railů (blue = highest, black = lowest, cyan = 65%) - needs further testing
- */
--- header.h
+++ header.h
@@ -2,74 +2,89 @@
 //slouzi k ukladani casu
 class vector {
 public:
-	unsigned long edgeUp;
-	unsigned long edgeDown;
+	unsigned long edgeUp; //time at which it goes from blue to black
+	unsigned long edgeDown; //time at which it goes from black to blue (or cyan to blue)
 	unsigned long pushTime;
 	unsigned long pullTime;
 
-	vector();
-
-	//speed calculation, prevState is gonna be "c" for shorter and "a" for longer ones (TBD1)
-	void vecSpeed(int l, int s);
+	vector(){}
+	//speed calculation, prevState is gonna be "c" for long stripes and "a" for normal ones
+	void vecSpeed1(int l1, int s);
+	void vecSpeed2(int l2, int s);
+	
 };
 
 class sensor {
 	int pinIn;
-	int pinOut;
+
 	int value;
-	//creates a "debouncer"
+	//creates a "debouncer" for the long stipes that have to be held down
 	int prevValue;
 	unsigned long prevTime;
 	//prevState is gonna be "c" for shorter rectangles and "a" for longer ones
 	char prevState; //a = blue, b = black, c = cyan
 
-	//vsechny elementy jsou nastaveny na 0
-	vector pole[5];
+	
 
 	//index pole
-	int i = 0;
+	int i;
 
 public:
 	sensor() {}
-	sensor(int in, int out);
-
-	//detekce hrany
+	//Vsimnete si, ze prevState je nastaven an "a", to je dulezite pro metodu sensor::start
+	sensor(int in, int out) : pinIn(in), pinOut(out) {prevState = 'a';}
+	int pinOut;
+	//vsechny elementy jsou nastaveny na 0
+	vector pole[5];
+	//Detekce hran, ukladani casu a posouvani pole
 	void edge(int upTrsh, int lowTrsh);
+	void start(int bttn, int upTrsh, int lowTrsh);
+	//Pouze pro debugging
 	void tisk(int i);
+	void vecSpeed(int i, int l1, int l2, int s);
+	
 };
 
 
 
-//metody vektoru
-vector::vector() {
-	edgeUp = 0;
-	edgeDown = 0;
-	pushTime = 0;
-	pullTime = 0;
-}
-//dodelat (TBD)
-void vector::vecSpeed(int l, int s) {
+//METODY VECTOR
+//dodelat (TBD) do mainu if(edgeDown > edgeUp)...
+void vector::vecSpeed1(int l1, int s) {
 	unsigned long v = 0;
-	//delta t
-	v = edgeDown - edgeUp;
-	v = l / v;
+	v = edgeDown - edgeUp; //delta t
+	v = l1 / v;
+	Serial.print("Rychlost: ");
+	Serial.println(v);
+	//saving the time of push and pull for normal rectangles
+	pushTime = edgeUp + (s / v);
+	pullTime = edgeDown + (s / v);
+	edgeDown = 0;
+	edgeUp = 0;
+}
+void vector::vecSpeed2(int l2, int s) {
+	unsigned long v = 0;
+	v = edgeDown - edgeUp; //delta t
+	v = l2 / v;
 
 	//saving the time of push and pull for normal rectangles
 	pushTime = edgeUp + (s / v);
-	pullTime = edgeUp + ((s + l) / v);
+	pullTime = NULL;
+	edgeDown = 0;
+	edgeUp = 0;
 }
 
 
-//metody sensoru
-sensor::sensor(int in, int out) {
-	pinIn = in;
-	pinOut = out;
-	value = 0;
-	prevState = "a";
-	prevValue = 0;
-	prevTime = 0;
-}
+//METODY SENSOR
+/*
+//Snimani stavu
+V main loop for cyklus hlida stavy jasu na senzorech, kazdy jednotlivy objekt senzoru
+pak zapisuji vlastni casy hran, pocita rychlost a zapisuje cas push/pull do pole vnoreneho objektu vektor
+na konci main loop se porovnavaji pull/push casy s realnym casem
+Vyjimkou je stav c (cyan) pro kratsi obdelniky (ktere se musi drzet) v tomto pripade zajistuje pull up
+primo tato metoda (sensor::edge)
+*/
 void sensor::edge(int upTrsh, int lowTrsh) {
+	//prevStates: a = blue, b = black, c = cyan
 
 	value = analogRead(pinIn);
 	if (i >= 5)
@@ -77,13 +92,13 @@ void sensor::edge(int upTrsh, int lowTrsh) {
 
 	if (value <= lowTrsh && prevState == 'a') {
 		//from blue to black
-		prevState = "b";
+		prevState = 'b';
 		pole[i].edgeUp = millis();
 	}
 	else if (value > lowTrsh && value < upTrsh && prevState == 'b' && value == prevValue) {
 		//from black to cyan
-		if (millis() - prevTime >= 10) {
-			prevState = "c";
+		if (millis() - prevTime >= 2) {
+			prevState = 'c';
 			pole[i].edgeDown = millis();
 
 			//inkrementuji pole vektoru
@@ -93,7 +108,7 @@ void sensor::edge(int upTrsh, int lowTrsh) {
 	}
 	else if (value >= upTrsh && prevState == 'b') {
 		//from black to blue
-		prevState = "a";
+		prevState = 'a';
 		pole[i].edgeDown = millis();
 
 		//inkrementuji pole vektoru
@@ -101,32 +116,112 @@ void sensor::edge(int upTrsh, int lowTrsh) {
 	}
 	else if (value >= upTrsh && prevState == 'c') {
 		//from cyan to blue
-		prevState = "a";
+		prevState = 'a';
 		digitalWrite(pinOut, LOW);
 	}
-	//promene pro debouncer
+
+	/*Promene pro "debouncer" pro useky kde se to musi drzet
+	Protoze arduino AD prevodnik meri hodne rychle, tak v podstate merime senzorem spojite!
+	Hodnota senzoru proto vzdycky projde hranici vyhrazenou pro dlouhe useky, ktere se musi drzet,
+	musime se ujistit, ze se hodnota na senzoru dale nemeni a opravdu se jedna o usek 'c'
+	*/
 	prevValue = value;
 	prevTime = millis();
 	}
+/*
+//startovaci sekvence
+Arduino main loop najde na ktere ctvrtine je obdelnik a nastavi na ni prevState = b, jako black
+potom startseq proklika kazdou ctvrtinu hraci plochy, protoze nemuze vedet kde je
+start policko
+*/
+void sensor::start(int bttn, int lowTrsh, int upTrsh) {
+	value = analogRead(pinIn);
+
+	if (value <= lowTrsh){
+		while (digitalRead(bttn)) {}
+		prevState = "b";
+		pole[0].edgeDown = millis();
+	}
+
+}
 
 void sensor::tisk(int i) {
 	Serial.print("Senzor ");
 	Serial.print(i);
 	Serial.print(" : ");
 	Serial.println(value);
+	Serial.println("|||||||||||||||||||||||||||");
+	for (int j = 0; j < 5; ++j) {
+		Serial.print("Edge up time: ");
+		Serial.println(pole[j].edgeUp);
+
+		Serial.print("Edge down time: ");
+		Serial.println(pole[j].edgeDown);
+
+		Serial.print("Push time: ");
+		Serial.println(pole[j].pushTime);
+
+		Serial.print("Pull time: ");
+		Serial.println(pole[j].pullTime);
+	}
+	Serial.println("|||||||||||||||||||||||||||");
+}
+
+void sensor::vecSpeed(int i, int l1, int l2, int s) {
+	if (pole[i].edgeDown > pole[i].edgeUp){
+
+		if (prevState == 'a') {
+			pole[i].vecSpeed1(l1, s);
+		}
+		else if (prevState == 'c') {
+			pole[i].vecSpeed2(l2, s);
+		}
+	
+	}
 }
 
 
 
-//Kalibrace momentalne casova, predelat na tlacitko
 /*
-(TBD)
-void calib() {
+Kalibrace senzoru pomoci tlacitka, nejdrive snima cernou, potom modrou pro vyvoreni intervalu <0,100>%
+svetle modra je asi 65% od cerne, tzn. nastavi se dve hranice pro vytvoreni trech stavu
+Tlacitko je pripojene na D12 pres pull up
+Pro zjednoduseni je poradi barev dulezite! Nejdriv cerna potom modra.
+*/
 
+void calib(int Sen, int Pin, int bttn, int *upTrsh, int *lowTrsh) {
+	
+	int tmp = 0;
+	digitalWrite(Pin, HIGH);
+	while (digitalRead(bttn)) {}
+	digitalWrite(Pin, LOW);
+	*lowTrsh = analogRead(Sen);
+	delay(500);
+
+	digitalWrite(Pin, HIGH);
+	while (digitalRead(bttn)) {}
+	digitalWrite(Pin, LOW);
+	*upTrsh = analogRead(Sen);
+	delay(500);
+
+	tmp = (*lowTrsh + *upTrsh) / 10;
+	Serial.println(*lowTrsh);
+	Serial.println(*upTrsh);
+	Serial.println(tmp);
+	*lowTrsh += 1 * tmp;
+	*upTrsh -= 3 * tmp;
+}
+//Proklikani vsech policek SCRAPPED
+/*void startseq(char *Pin) {
+	for (int i = 0; i < 4; ++i) {
+		digitalWrite(*(Pin + i), HIGH);
+		delay(10);
+		digitalWrite(*(Pin + i), LOW);
+	}
 }
 */
 /*
-Finish up the speed calc. for shorter rectangles (see TBD1) and pushTime pullTime calculations. Shorter rectangles don't need a pullTime
-Figure out the calibration process using a button. First rectangle is right under the sensor so maybe add starting time for the first one? Also find out how to position the treshold rails.
+
+
 Throw a loop into main to go through all four sensors and all of their timings.
 */
